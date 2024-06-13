8fd9496d47a4d3175207b15c92941132307c5807
Sonia Chagusia <navalayochagusia@gmail.com>
2024-06-13 05:03:04 +0000

    debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# fix to increase requetst limit to our server
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
